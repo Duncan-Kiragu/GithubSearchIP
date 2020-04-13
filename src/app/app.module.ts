8bb48eb002e4224a64625cec85ac6641a3914024
Duncan-Kiragu <mathenge089@gmail.com>
2020-04-13 17:08:45 +0300

    defining imports in the app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { RouterModule, Routes } from '@angular/router'
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+import { UserService } from './user-http/user.service';
+import { ReposService } from './repos-http/repos.service';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RoutingModule} from './routing/routing.module'
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './display/display.component';
 import { RepositoryComponent } from './repository/repository.component';
+import { DateCountPipe } from './date-count.pipe';
 import { HighlightDirective } from './highlight.directive';
 import { ForksPipe } from './forks.pipe';
-import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -20,9 +27,14 @@ import { DateCountPipe } from './date-count.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RoutingModule,
+    HttpClientModule,
+    NgProgressModule,
+    NgProgressHttpModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [UserService, ReposService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
